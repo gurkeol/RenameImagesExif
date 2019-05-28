b3a52f7cd5dd6cb7d9b3db6369eec6d728f0c82b
Markus Fromme <markus.fromme@btc-ag.com>
2019-05-28 18:00:46 +0200

    Erstes Commit

---
+++ RenameImagesExif.py
@@ -0,0 +1,51 @@
+import argparse
+import exifread
+import os
+
+MAKETRANS = str.maketrans({":": "", " ": ""})
+
+
+def get_arguments():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-f", "--type", type=str, required=True, help="file type - e.g. jpg")
+    arg_parser.add_argument("-d", "--directory", type=str, required=True, help="working directory - e.g. /home/user")
+    return arg_parser.parse_args()
+
+
+def process_directory(directory):
+    print("process directory \"{}\"".format(directory))
+    for filename in os.listdir(directory):
+        if os.path.isdir(filename):
+            process_directory(filename)
+        else:
+            if filename.lower().endswith(args.type):
+                rename_file(filename)
+            else:
+                print("ignore file \"{}\"".format(filename))
+
+
+def get_exif_datetimeoriginal_as_str(filename):
+    with open(filename, "rb") as file:
+        tags = exifread.process_file(file, stop_tag="DateTimeOriginal", details=False)
+    return str(tags["EXIF DateTimeOriginal"])
+
+
+def get_new_filename(filename):
+    creation_date = get_exif_datetimeoriginal_as_str(filename)
+    return creation_date.translate(MAKETRANS) + "." + args.type
+
+
+def rename_file(old_filename):
+    try:
+        new_filename = get_new_filename(old_filename)
+        if new_filename == old_filename:
+            print("file \"{}\" already renamed".format(old_filename))
+        else:
+            os.rename(old_filename, new_filename)
+            print("file \"{}\" renamed to \"{}\"".format(old_filename, new_filename))
+    except Exception as e:
+        print("error processing file \"{}\": {}".format(old_filename, e))
+
+
+args = get_arguments()
+process_directory(args.directory)
